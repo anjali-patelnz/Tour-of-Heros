768826656dbb939ba76b233701e000827d42f318
Anjali Patel <anjali.patel@lawvu.co>
2021-11-12 10:47:44 +1300

    Enable http services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    HttpClientModule,
   ],
   bootstrap: [AppComponent],
 })
